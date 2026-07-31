cdd1dfb670881098cf0152f8b49a6c784871e53c
johnnychapman2000 <johnnychapman2000@gmail.com>
2026-07-31 16:51:36 -0400

    Create workoutdata.js

---
+++ netlify/functions/workoutdata.js
@@ -0,0 +1,25 @@
+exports.handler = async function () {
+  try {
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbz8QUcg9I5DIqAHyN2Vx0t_xODRPZApqtc3bQJm3PSaUn9vuG7-1Q1pNlzISBkBPmM4NA/exec"
+    );
+
+    const data = await response.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: data
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message
+      })
+    };
+  }
+};
